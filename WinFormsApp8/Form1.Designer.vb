ce49f154f6d3edc5b1208216962a5396d3c51886
Mahadevii <mshadrina@gmail.com>
2023-06-26 06:30:58 +0500

    Добавьте файлы проекта.

---
+++ WinFormsApp8/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Button1 = New Button()
+        ListView1 = New ListView()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 21)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(12, 54)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 2
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(12, 87)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 3
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(28, 120)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 4
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ListView1
+        ' 
+        ListView1.Location = New Point(224, 12)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(564, 375)
+        ListView1.TabIndex = 5
+        ListView1.UseCompatibleStateImageBehavior = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 399)
+        Controls.Add(ListView1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListView1 As ListView
+End Class
---
+++ WinFormsApp8/Form1.vb
@@ -0,0 +1,74 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.PlaceholderText = "Сумма вклада"
+        TextBox2.PlaceholderText = "Процент"
+        TextBox3.PlaceholderText = "Срок"
+        Button1.Text = "Рассчитать"
+        With ListView1
+            .View = View.Details
+            .FullRowSelect = True
+            .Columns.Add("Год")
+            .Columns.Add("Сумма")
+        End With
+        Maximum()
+    End Sub
+
+    Private Sub Calc(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim P = Val(TextBox1.Text)
+        Dim r = Val(TextBox2.Text) / 100
+        Dim n = Val(TextBox3.Text)
+        ListView1.Items.Add(New ListViewItem({0, P}))
+        For i = 1 To n
+            Dim A = P * Math.Pow(1 + r / 12, i * 12)
+            ListView1.Items.Add(New ListViewItem({i, A}))
+        Next i
+
+    End Sub
+    Sub Maximum()
+        Dim X As Single, Y As Single, Max As Single
+        X = InputBox("Введите первое число", "Ввод чисел для сравнения")
+        Y = InputBox("Введите второе число", "Ввод чисел для сравнения")
+        Select Case X > Y
+            Case True
+                Max = X
+            Case False
+                Max = Y
+        End Select
+        MsgBox("Наибольшее из двух чисел: " & Max, , "Результат сравнения")
+    End Sub
+
+    ''' <summary>
+    ''' Пример алгоритма с Case"
+    ''' </summary>
+    Sub Goroskop()
+        Dim Year As Integer
+        Year = InputBox("Введите год", "Организация ввода")
+        Select Case Year Mod 12
+            Case 0
+                MsgBox("Год Обезьяны", , "Результат")
+            Case 1
+                MsgBox("Год Петуха", , "Результат")
+            Case 2
+                MsgBox("Год Собаки", , "Результат")
+            Case 3
+                MsgBox("Год Свиньи", , "Результат")
+            Case 4
+                MsgBox("Год Крысы", , "Результат")
+            Case 5
+                MsgBox("Год Коровы", , "Результат")
+            Case 6
+                MsgBox("Год Тигра", , "Результат")
+            Case 7
+                MsgBox("Год Зайца", , "Результат")
+            Case 8
+                MsgBox("Год Дракона", , "Результат")
+            Case 9
+                MsgBox("Год Змеи", , "Результат")
+            Case 10
+                MsgBox("Год Лошади", , "Результат")
+            Case 11
+                MsgBox("Год Овцы", , "Результат")
+            Case 12
+        End Select
+    End Sub
+End Class
